d8bc4bb37991e21f1810e0123de54f5dfa3340cd
Mason <rosenaumason@gmail.com>
2023-06-05 14:47:51 -0700

    Test: Chart is correctly generated

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+it("Chart is generated and displayed", function () {
+  //visit home page
+  cy.visit("/")
+
+  //click Line link & verify URL redirection
+  cy.findByRole("link", {name: "Line"}).click()
+  cy.url().should("eq", "http://localhost:8080/line.html")
+
+  //fill the line chart with labes & data
+  cy.fillChartData()
+  
+  //verify image doesn't exist before
+  cy.get("#chart-img").should("not.exist")
+
+  //click Generate chart
+  cy.get("#generate-chart-btn").click()
+
+  //verify image exists and is visible to user
+  cy.get("#chart-img")
+    .should("exist")
+    .and("be.visible")
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,21 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+require("@testing-library/cypress/add-commands")
+
+Cypress.Commands.add("fillChartData", function () {
+    cy.get("#chart-title-input").type("Cookies vs. Brownies")
+    cy.get("#x-label-input").type("Cookies")
+    cy.get("#y-label-input").type("Brownies")
+  
+    cy.get("#x-y-data-grid").children().eq(3).type("1")
+    cy.get("#x-y-data-grid").children().eq(4).type("4")
+    cy.get('#add-values-btn').click()
+  
+    cy.get("#x-y-data-grid").children().eq(5).type("2")
+    cy.get("#x-y-data-grid").children().eq(6).type("8")
+    cy.get('#add-values-btn').click()
+  
+    cy.get("#x-y-data-grid").children().eq(7).type("3")
+    cy.get("#x-y-data-grid").children().eq(8).type("16")
+})
